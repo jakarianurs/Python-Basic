ec420ee90914869be6abc98e6646837c19b8f021
Md Jakaria Nur <122019360+mdjakarianurs@users.noreply.github.com>
2024-06-01 19:59:06 +0600

    Create Python Operator.py

---
+++ Python Operator.py
@@ -0,0 +1,9 @@
+print(5 + 4 - 7 + 3)
+
+"""
+Additions and subtractions have the same precedence, and we need to calculate from left to right.
+The calculation above reads:
+5 + 4 = 9
+9 - 7 = 2
+2 + 3 = 5
+"""
