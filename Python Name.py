8f19af7a5cad287e29f4a67ae5b5b142fea59a56
Md Jakaria Nur <122019360+mdjakarianurs@users.noreply.github.com>
2024-06-01 19:56:57 +0600

    Create Python Name.py

---
+++ Python Name.py
@@ -0,0 +1,14 @@
+myvar = "Md Jakaria Nur"
+my_var = "Md Jakaria Nur"
+_my_var = "Md Jakaria Nur"
+myVar = "Md Jakaria Nur"
+MYVAR = "Md Jakaria Nur"
+myvar2 = "Md Jakaria Nur"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
